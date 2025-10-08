48790ac1e776867209dd8027096de5afd1ac6342
Thushan <153522711+ThushanDeS@users.noreply.github.com>
2025-10-08 12:39:24 +0530

    update station model and add contoller with several endpoints

--- assignment-10-web-main/pulsecharge/Models/Station.cs
+++ assignment-10-web-main/pulsecharge/Models/Station.cs
@@ -6,30 +6,30 @@ namespace pulsecharge.Models
 {
     public class Station
     {
-        [BsonId]
-        public ObjectId Id { get; set; }
-
+        [BsonId] public ObjectId Id { get; set; }
         public string Name { get; set; } = default!;
-
         public Location Location { get; set; } = default!;
-
         public string Type { get; set; } = "AC";
-
         public int SlotCount { get; set; }
-
+        public bool Active { get; set; } = true;
+        public List<ObjectId> OperatorIds { get; set; } = new();
+        public Hours Hours { get; set; } = new() { Open = "09:00", Close = "21:00" };
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public class Hours
+    {
+        public string Open { get; set; } = "09:00";
+        public string Close { get; set; } = "21:00";
     }
 
     public class Location
     {
         public double Lat { get; set; }
-
         public double Lng { get; set; }
-
         public string? Address { get; set; }
-
         public GeoJsonPoint<GeoJson2DGeographicCoordinates> GeoJson
-            => new GeoJsonPoint<GeoJson2DGeographicCoordinates>(
-                new GeoJson2DGeographicCoordinates(Lng, Lat));
+            => new GeoJsonPoint<GeoJson2DGeographicCoordinates>(new GeoJson2DGeographicCoordinates(Lng, Lat));
     }
 }
