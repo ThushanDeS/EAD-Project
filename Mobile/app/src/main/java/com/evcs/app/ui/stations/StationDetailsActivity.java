0636da67148985e3eb75331fdea9b7a99aa8eb8a
Thushan <153522711+ThushanDeS@users.noreply.github.com>
2025-10-10 00:43:41 +0530

    Add StationsFragment, StationDetailsActivity, and related layouts

---
+++ Mobile/app/src/main/java/com/evcs/app/ui/stations/StationDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.evcs.app.ui.stations;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.evcs.app.R;
+
+public class StationDetailsActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_details);
+        
+        // TODO: Implement station details functionality
+    }
+}
